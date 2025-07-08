a7edf519460c5964822a3f9c12c35a55426fa690
Joel <garciacervantesj00@gmail.com>
2025-07-07 23:34:30 -0700

    js calc project

--- error-handling/index.js
+++ error-handling/index.js
@@ -1,9 +1,43 @@
 // error handling in JS
 
 
-// erros happen due to:
+// errors happen due to:
 // NETWROK ERRORS
 // SECURITY REJECTION
-console.log("Hello");
 
+try {
+    console.log("hi");
+
+}
+catch (error) {
+    console.error(error);
+}
+
+//optional
+finally {
+    // CLOSE FILES
+    // CLOSE CONNECTIONS
+    // RELEASE RESOURCES
+    console.log("always executes");
+}
+console.log("end");
+
+try {
+    const dividend = Number(window.prompt("enter dividend"));
+    const divisor = Number(window.prompt("Enter diviser"));
+
+    if (divisor == 0) {
+        throw new Error("No division by zero");
+    }
+
+    if (isNaN(dividend) || isNaN(divisor)) {
+        throw new Error("values must be numbers");
+    }
+
+    const result = dividend / divisor;
+    console.leg(result);
+}
+catch (error) {
+    console.error(error);
+}
 console.log("end");
